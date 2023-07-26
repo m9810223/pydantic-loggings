1d71713eaea2d2739ba9915262dbc06744094804
m9810223 <m9810223@gmail.com>
2023-07-27 01:57:31 +0800

    feat: complete rich logging

--- src/pydantic_loggings/rich/handlers.py
+++ src/pydantic_loggings/rich/handlers.py
@@ -1,20 +1,18 @@
 import logging
 import typing as t
 
-import rich.logging
 from pydantic import Field
 
 from .. import not_set
 from .. import utils
-
-
-rich.logging.RichHandler
+from .formatters import Formatter
 
 
 class Handler(not_set.Handler):
     NAME: t.ClassVar[str] = 'rich'
     class_: str = Field(default='rich.logging.RichHandler', alias='()')
     level: t.Optional[str] = utils.get_level_name(logging.DEBUG)
+    formatter: t.Optional[str] = Formatter.NAME
     #
     omit_repeated_times: t.Optional[bool] = False
     log_time_format: t.Optional[str] = '%m-%d %H:%M:%S'
